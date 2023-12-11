c4bff591da6b32b4f2e0f210e493390d2ad39a46
Gregorio-Moreta <78994816+Gregorio-Moreta@users.noreply.github.com>
2023-12-11 15:54:40 -0500

    deleting from the nth place in an array

--- Arrays/deleting_from_th_place_of_array.py
+++ Arrays/deleting_from_th_place_of_array.py
@@ -10,3 +10,36 @@ def removeMiddle(arr, i, length):
 arr = [1, 2, 3, 4, 5]
 
 print(removeMiddle(arr, 5, 5))
+
+# In the worst case, n - 1 shifts may be required. Therefore, the code above is O(n).
+
+#  Below are the outputs when I change the value of the second parameter 'i' in the removeMiddle function
+# The function iterates starting on the 'i' value which is then used to remove the value at index 'i' then it will shift the elements to the left
+
+# 0 index
+# [2, 2, 3, 4, 5]
+# [2, 3, 3, 4, 5]
+# [2, 3, 4, 4, 5]
+# [2, 3, 4, 5, 5]
+# None
+
+# 1 Index
+# [1, 3, 3, 4, 5]
+# [1, 3, 4, 4, 5]
+# [1, 3, 4, 5, 5]
+# None
+
+# 2 Index
+# [1, 2, 4, 4, 5]
+# [1, 2, 4, 5, 5]
+# None
+
+# 3 index
+# [1, 2, 3, 5, 5]
+# None
+
+# 4 index 
+# None
+
+# 5 index
+# None
